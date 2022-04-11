e5ae4f24600e139de084018250916aca06c08ea4
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-11 17:38:41 +0530

    Given 3integers finding max and passing test cases

--- Program.cs
+++ Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace FindingMax
+namespace FindMaxValue
 {
-    class Program
+
+    public class Program
     {
+        public int FindMaximum(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("All three having same number");
+                return -1;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to finding max integers program!");
+            Program obj = new Program();
+            int maxValue = obj.FindMaximum(70, 35, 56);
+            Console.WriteLine(maxValue);
+            Console.ReadLine();
         }
+
     }
+
 }
