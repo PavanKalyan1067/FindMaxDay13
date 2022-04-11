226e3db0cfc134c98c27863aa74d7e1fdb9c0fde
Pavan Kalyan <pavankalyanyadav321@gmail.com>
2022-04-11 17:59:13 +0530

    Sorting out

---
+++ ArrayClassBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+namespace FindMaxValue
+{
+    public class ArrayClassBase
+    {
+        private IEnumerable<object> inputArray;
+
+        public void ToPrint()
+        {
+            foreach (var ele in inputArray)
+            {
+                Console.Write(ele + " ");
+            }
+        }
+    }
+}
